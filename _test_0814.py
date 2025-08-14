0aef5a4dcd2fdb323eda5a70ec7ad4ee3db32934
ngoo <ngoo@example.com>
2025-08-14 11:57:22 +0800

    _test_0814 v2.0.2

--- _test_0814.py
+++ _test_0814.py
@@ -5,3 +5,6 @@ from tensorflow.keras.datasets import mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 print(x_train.shape)
 print(x_test.shape)
+
+cv2.imshow("win", x_train[0])
+cv2.waitKey(0)
