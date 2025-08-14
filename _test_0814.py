a77e0aff8000651ca110abab32df42108de1a21b
ngoo <ngoo@example.com>
2025-08-14 13:51:01 +0800

    _test_0814 v2.0.7

--- _test_0814.py
+++ _test_0814.py
@@ -1,18 +1,26 @@
 import numpy as np
 import cv2
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
 new_x_train = x_train.astype("float32")/255.0
 new_x_test = x_test.astype("float32")/255.0
 
+new_y_train = to_categorical(y_train)
+new_y_test = to_categorical(y_test)
+
+print(new_y_train[0])
+
+"""
 print(x_train.shape)
 print(x_test.shape)
 
-print(y_train[0])
+
 
 for i in range(10):
     img = cv2.resize(x_train[i],(280,280))
     cv2.imshow("win", img)
     cv2.waitKey(0)
+"""
